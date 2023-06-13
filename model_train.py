be47d9e0da34a519d8f23d16c997ffa974f3dafd
A P Aishwarya Lakshmi <77623254+aishu19-dotcom@users.noreply.github.com>
2023-06-13 21:26:01 +0530

    task 4

---
+++ model_train.py
@@ -0,0 +1,101 @@
+
+# 1) IMPORTING PACKAGES
+# The first thing you should always do in a Python file is to import any packages that you will need within the file. 
+
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import StandardScaler
+
+# 2) DEFINE GLOBAL CONSTANTS
+# Constants are variables that should remain the same througout the entire running of the module.
+# You should define these after the imports at the top of the file.
+# You should give global constants a name and ensure that they are in all upper
+# K is used to define the number of folds that will be used for cross-validation
+K = 10
+
+# Split defines the % of data that will be used in the training sample
+SPLIT = 0.75
+
+#3) ALGORITHM CODE
+# Next, we should write our code that will be executed when a model needs to be trained. 
+# There are many ways to structure this code and it is your choice how you wish to do this.
+# The code in the 'module_helper.py' file will break the code down into independent functions, which is 1 option. 
+
+# Load data
+def load_data(path: str = "/path/to/csv/"):
+   
+
+    df = pd.read_csv(f"{path}")
+    df.drop(columns=["Unnamed: 0"], inplace=True, errors='ignore')
+    return df
+
+# Create target variable and predictor variables
+def create_target_and_predictors(
+    data: pd.DataFrame = None, 
+    target: str = "estimated_stock_pct"
+):
+
+    # Check to see if the target variable is present in the data
+    if target not in data.columns:
+        raise Exception(f"Target: {target} is not present in the data")
+    
+    X = data.drop(columns=[target])
+    y = data[target]
+    return X, y
+
+# Train algorithm
+def train_algorithm_with_cross_validation(
+    X: pd.DataFrame = None, 
+    y: pd.Series = None
+):
+
+    # Create a list that will store the accuracies of each fold
+    accuracy = []
+
+    # Enter a loop to run K folds of cross-validation
+    for fold in range(0, K):
+
+        # Instantiate algorithm and scaler
+        model = RandomForestRegressor()
+        scaler = StandardScaler()
+
+        # Create training and test samples
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=SPLIT, random_state=42)
+
+        # Scale X data, we scale the data because it helps the algorithm to converge
+        # and helps the algorithm to not be greedy with large values
+        scaler.fit(X_train)
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        # Train model
+        trained_model = model.fit(X_train, y_train)
+
+        # Generate predictions on test sample
+        y_pred = trained_model.predict(X_test)
+
+        # Compute accuracy, using mean absolute error
+        mae = mean_absolute_error(y_true=y_test, y_pred=y_pred)
+        accuracy.append(mae)
+        print(f"Fold {fold + 1}: MAE = {mae:.3f}")
+
+    # Finish by computing the average MAE across all folds
+    print(f"Average MAE: {(sum(accuracy) / len(accuracy)):.2f}")
+ 
+# 4) MAIN FUNCTION
+# Your algorithm code should contain modular code that can be run independently.
+# You may want to include a final function that ties everything together, to allow the entire pipeline of loading the data and training the algorithm to be run all at once
+
+# Execute training pipeline
+def run():
+    
+    # Load the data first
+    df = load_data()
+
+    # Now split the data into predictors and target variables
+    X, y = create_target_and_predictors(data=df)
+
+    # Finally, train the machine learning model
+    train_algorithm_with_cross_validation(X=X, y=y)
